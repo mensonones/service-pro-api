5fe44e62926912267d7b30e309f2bc0254938dc1
mensones <mensones.1@gmail.com>
2024-06-29 16:58:31 -0300

    estudado e aplicado algumas opcoes de personalizacao com o admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Servico, Cliente, Profile, MeioPagamento, Atendimento, Prestador
 
 
@@ -10,20 +10,48 @@ class ServicoAdmin(admin.ModelAdmin):
     search_fields = ('nome',)
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tipo', 'telefone', 'email')
+# callable
+@admin.display(description="Name")
+def upper_case_name(obj):
+    return f"{obj.user.username}".upper()
+
+
+# Criei essa classe em comum para CLiente e Prestador para evitar duplicacao de codigo
+# @admin.register(Cliente, Prestador)
+class ProfileAdminBase(admin.ModelAdmin):
+    list_display = (upper_case_name, 'telefone')
+    # https://docs.djangoproject.com/en/5.0/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display_links
+    list_display_links = ['telefone']
     search_fields = ('user__username', 'email')
+    fieldsets = (
+        ('Informações', {
+            'fields': ('user', 'telefone', 'email',),
+        }),
+        ('Endereço', {
+            'fields': ('rua', 'bairro', 'numero', 'cidade', 'estado', 'cep', 'pais',),
+            'classes': ('collapse',),
+        }),
+    )
+
+    def get_username(self, obj):
+        return obj.user.username  # Ou use obj.user.get_full_name() se preferir o nome completo
+
+    get_username.short_description = 'Username'  # Define a descrição do cabeçalho da coluna no admin
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
-""" @admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tipo', 'telefone', 'email') """
+# @admin.register(Profile)
+@admin.register(Cliente)
+class ClienteAdmin(ProfileAdminBase):
+    list_display = (upper_case_name, 'telefone', 'email')
 
 
-""" @admin.register(Prestador)
-class PrestadorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tipo', 'telefone', 'email') """
+@admin.register(Prestador)
+class PrestadorAdmin(ProfileAdminBase):
+    # list_display = ('user', 'telefone', 'email')
+    pass
 
 
 @admin.register(MeioPagamento)
@@ -32,7 +60,49 @@ class MeioPagamentoAdmin(admin.ModelAdmin):
     search_fields = ('nome',)
 
 
+# Adicionando ações ao ModelAdmin (https://docs.djangoproject.com/en/5.0/ref/contrib/admin/actions/)
+@admin.action(description='Atendimento pago')
+def atendimento_concluido(modeladmin, request, queryset):
+    queryset.update(status=Atendimento.STATUS_CONCLUIDO)
+
+
+# Adicionando acoes com tratamento de acao
+# (https://docs.djangoproject.com/en/5.0/ref/contrib/admin/actions/#handling-errors-in-actions)
+@admin.action(description='Atendimento cancelado')
+def atendimento_cancelado(modeladmin, request, queryset):
+    nao_agendado = queryset.exclude(status=Atendimento.STATUS_AGENDADO)
+    if nao_agendado.exists():
+        modeladmin.message_user(
+            request,
+            f'{nao_agendado.count()} atendimentos nao podem ser cancelados porque nao estao com status: Agendado',
+            messages.ERROR
+        )
+    agendados = queryset.filter(status=Atendimento.STATUS_AGENDADO)
+    total_agendados = agendados.update(status=Atendimento.STATUS_CANCELADO)
+
+    if total_agendados > 0:
+        modeladmin.message_user(
+            request,
+            f'{total_agendados} atendimentos foram cancelados com sucesso.',
+            messages.SUCCESS
+        )
+    else:
+        modeladmin.message_user(
+            request,
+            'Nenhum atendimento foi cancelado.',
+            messages.WARNING
+        )
+
+
 @admin.register(Atendimento)
 class AtendimentoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'data_hora'
     list_display = ('servico', 'cliente', 'prestador_servico', 'data_hora', 'valor', 'meio_pagamento', 'status')
     search_fields = ('servico__nome', 'cliente__user__username', 'prestador_servico__user__username')
+    readonly_fields = ('valor',)
+    actions = [atendimento_concluido, atendimento_cancelado]
+
+
+# desativando acoes em todo o site
+#  (https://docs.djangoproject.com/en/5.0/ref/contrib/admin/actions/#django.contrib.admin.AdminSite.disable_action)
+admin.site.disable_action("delete_selected")
--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import EmailValidator, MinValueValidator, MaxValueValidator, RegexValidator
 from django.db import models
 from rest_framework.authtoken.admin import User
@@ -36,7 +37,7 @@ class Endereco(models.Model):
         return f"{self.rua}, {self.numero}, {self.bairro}, {self.cidade}, {self.estado}, {self.pais}"
 
 
-class Profile(Endereco, models.Model):
+class Profile(Endereco):
     """
     Uso de herenca multipla para herdar os campos de Endereco
     """
@@ -62,6 +63,9 @@ class Profile(Endereco, models.Model):
             pass
         return super().save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = 'Perfil'
+
     def __str__(self):
         return f'{self.user.username} ({self.tipo})'
 
@@ -86,11 +90,13 @@ class Prestador(Profile):
     objects = PrestadorManager()
 
     def save(self, *args, **kwargs):
-        # ...
+        if self.id is None:
+            self.tipo = Profile.TIPO_PRESTADOR
         return super().save(*args, **kwargs)
 
     class Meta:
         proxy = True
+        verbose_name_plural = 'Prestadores'
 
 
 class ClienteManager(models.Manager):
@@ -102,7 +108,8 @@ class Cliente(Profile):
     objects = ClienteManager()
 
     def save(self, *args, **kwargs):
-        self.tipo = Profile.TIPO_CLIENTE
+        if self.id is None:
+            self.tipo = Profile.TIPO_CLIENTE
         return super().save(*args, **kwargs)
 
     class Meta:
@@ -132,9 +139,38 @@ class Atendimento(models.Model):
     prestador_servico = models.ForeignKey(Prestador, on_delete=models.CASCADE,
                                           related_name='atendimentos_prestador')
     data_hora = models.DateTimeField()
-    valor = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
+    valor = models.DecimalField(max_digits=8,
+                                decimal_places=2,
+                                validators=[MinValueValidator(0)],
+                                blank=True)
     meio_pagamento = models.ForeignKey(MeioPagamento, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=CHOICES_STATUS)
+
+    def is_new_attendance(self):
+        return self.id is None
+
+    def _load_value_from_service(self):
+        if self.is_new_attendance():
+            self.valor = self.servico.valor
+        elif self.status == Atendimento.STATUS_AGENDADO and self.servico.valor < self.valor:
+            self.valor = self.servico.valor
+
+    def _validate_status_change(self):
+        if not self.is_new_attendance():
+            instancia_do_banco = Atendimento.objects.get(id=self.id)
+
+            # self -> o objeto a ser salvo
+            # instancia_do_banco -> o objeto já salvo
+
+            if (instancia_do_banco.status in (Atendimento.STATUS_CONCLUIDO,
+                                              Atendimento.STATUS_CANCELADO)
+                    and instancia_do_banco.status != self.status):
+                raise ValidationError('Não é possível atualizar o status de um atendimento finalizado.', 1001)
+
+    def save(self, *args, **kwargs):
+        self._load_value_from_service()
+        self._validate_status_change()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Atendimento de {self.servico} para {self.cliente} por {self.prestador_servico} em {self.data_hora}"
